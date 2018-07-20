0ead6c4e5404059c6f29993e4d1d18c5d16c16d8
Matt Tuazon <sierra@Macs-Mac.local>
2018-07-19 23:56:32 -0400

    added different target number every round
    allow rebasing

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -16,8 +16,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-        targetValue =  1 + Int(arc4random_uniform(101))
+        startNewRound()
+        
+//        currentValue = lroundf(slider.value)
+//        targetValue =  1 + Int(arc4random_uniform(100))
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -38,14 +41,18 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ sender: UISlider) {
         currentValue = lroundf(sender.value)
         print("The slider's current value is:  \(sender.value) ") 
     }
-    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
     
 }
 
