a93ca70e303722f95c9b7199653e19453acc838b
Matthew Tuazon <matttuazon106@gmail.com>
2018-07-19 21:44:55 -0400

    Completed most of UI, working on functionalities of everything. Working slider with value recognition and working target value recognition.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        targetValue =  1 + Int(arc4random_uniform(101))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,17 +28,24 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert(){
         //alert message's message
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert
         )
         
         //alert message's button user's tap on, as a confirmation to seeing the message.
-        let action = UIAlertAction(title:"Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title:"OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
     }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        currentValue = lroundf(sender.value)
+        print("The slider's current value is:  \(sender.value) ") 
+    }
+    
     
 }
 
