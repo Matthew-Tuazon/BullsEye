3fd5a898cf7053ad185fff6ed931eaf86cf0eddf
Matt Tuazon <sierra@Macs-Mac.local>
2018-07-20 21:23:40 -0400

    Allow for target label to be updated after each round.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,16 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    
     var currentValue: Int = 0
     var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewRound()
-        
-//        currentValue = lroundf(slider.value)
-//        targetValue =  1 + Int(arc4random_uniform(100))
-        
+        updateLabels()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -42,16 +42,21 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
         startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ sender: UISlider) {
         currentValue = lroundf(sender.value)
         print("The slider's current value is:  \(sender.value) ") 
     }
-    func startNewRound(){
+    func startNewRound(){ //randomizes target Value from 1-100, default value at 50 at start, slider value equals where one places it
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() { 
+        targetLabel.text = "\(targetValue)"
     }
     
 }
